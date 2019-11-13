f88ec6de33aca8c9c69fd229e0e4609b5e27eafc
Talya Sasek <talyaercag@gmail.com>
2019-11-12 19:53:16 -0800

    Adds a favorites button

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,47 +1,51 @@
-import React, { Component } from 'react';
-import '../css/LandingPage.css';
-import CategoryContainer from './CategoryContainer.jsx';
-import VenueContainer from './VenueContainer.jsx';
-import LoginPage from '../components/LoginPage.jsx';
-import SignUpPage from '../components/SignUpPage.jsx';
+import React, { Component } from "react";
+import "../css/LandingPage.css";
+import CategoryContainer from "./CategoryContainer.jsx";
+import VenueContainer from "./VenueContainer.jsx";
+import LoginPage from "../components/LoginPage.jsx";
+import SignUpPage from "../components/SignUpPage.jsx";
+import axios from "axios";
 
 class MainContainer extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       // stateful components used for search bar and results
-      location: '',
-      searchInput: '',
+      location: "",
+      searchInput: "",
       searchResults: [],
-      
+
       // components used for map display
-      latitude: '',
-      longitude: '',
+      latitude: "",
+      longitude: "",
 
       // components related to selecting specific venue
-      venueId: '',
-      venueName: '',
-      venueUrl: '',
-      venueImage: '',
-      venueLocation: '',
-      venuePhone: '',
-      venueLatitude: '',
-      venueLongitude: '',
+      venueId: "",
+      venueName: "",
+      venueUrl: "",
+      venueImage: "",
+      venueLocation: "",
+      venuePhone: "",
+      venueLatitude: "",
+      venueLongitude: "",
       waitTime: 0,
       venueWaitTimeList: [],
 
       // components for infinite scrolling functionality
       current: 25,
       total: 50,
-      
+
       // components for conditional rendering of containers
       loginPage: false,
       signupPage: false,
       homePage: true,
       categoryPage: false,
       venuePage: false,
-    }
+
+      // components for favoriting restaurants
+      favorites: []
+    };
 
     this.loginButton = this.loginButton.bind(this);
     this.signupButton = this.signupButton.bind(this);
@@ -49,6 +53,7 @@ class MainContainer extends Component {
     this.setLocation = this.setLocation.bind(this);
     this.setSearchInput = this.setSearchInput.bind(this);
     this.search = this.search.bind(this);
+    this.addToFavorites = this.addToFavorites.bind(this);
 
     this.selectVenue = this.selectVenue.bind(this);
     this.setWaitTime = this.setWaitTime.bind(this);
@@ -62,8 +67,8 @@ class MainContainer extends Component {
       signupPage: false,
       homePage: false,
       categoryPage: false,
-      venuePage: false,
-    })    
+      venuePage: false
+    });
   }
   signupButton() {
     this.setState({
@@ -71,8 +76,8 @@ class MainContainer extends Component {
       signupPage: true,
       homePage: false,
       categoryPage: false,
-      venuePage: false,
-    })
+      venuePage: false
+    });
   }
 
   // functions used for search bar
@@ -87,62 +92,101 @@ class MainContainer extends Component {
 
   search() {
     // console.log('THIS STATE LOCATION : ', this.state.location);
-    fetch ('/api', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({searchInput: this.state.searchInput, location: this.state.location})
+    fetch("/api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        searchInput: this.state.searchInput,
+        location: this.state.location
+      })
     })
-      .then(response => response.json())  
+      .then(response => response.json())
       .then(data => {
         const parsedData = JSON.parse(data);
         // console.log('PARSEDDATA: ', parsedData);
         // console.log('introspecting the data: ', parsedData.businesses[0])
 
         // Coordinates used for map rendered in Category Container (List Page)
-        const firstBusinessLatitude = parsedData.businesses[0].coordinates.latitude;
-        const firstBusinessLongitude = parsedData.businesses[0].coordinates.longitude;
-        
+        const firstBusinessLatitude =
+          parsedData.businesses[0].coordinates.latitude;
+        const firstBusinessLongitude =
+          parsedData.businesses[0].coordinates.longitude;
+
         const listOfBusinesses = [];
         // console.log(parsedData.businesses.length)
         if (this.state.current <= 50) {
           for (let i = 0; i < this.state.current; i += 1) {
             // console.log('LIST BUSINESSES -> ', listOfBusinesses)
             listOfBusinesses.push({
-              id: parsedData.businesses[i].id, 
-              name: parsedData.businesses[i].name, 
-              image: parsedData.businesses[i].image_url, 
+              id: parsedData.businesses[i].id,
+              name: parsedData.businesses[i].name,
+              image: parsedData.businesses[i].image_url,
               location: parsedData.businesses[i].location,
               category: parsedData.businesses[i].categories[0].title,
               latitude: parsedData.businesses[i].coordinates.latitude,
               longitude: parsedData.businesses[i].coordinates.longitude
             });
           }
-  
+
           // this.setState({ latitude: firstBusinessLatitude.toString(), longitude: firstBusinessLongitude.toString() })
-  
+
           this.setState(state => {
             return {
               latitude: firstBusinessLatitude.toString(),
               longitude: firstBusinessLongitude.toString(),
               searchResults: listOfBusinesses,
-              current: state.current + 5,
-
-            }
-          })
+              current: state.current + 5
+            };
+          });
+          // console.log(this.state.searchResults);
         }
-      })
-      this.setState({
-        loginPage: false,
-        signupPage: false,
-        homePage: false,
-        categoryPage: true,
-        venuePage: false,
-      })    
-    
+      });
+    this.setState({
+      loginPage: false,
+      signupPage: false,
+      homePage: false,
+      categoryPage: true,
+      venuePage: false
+    });
+  }
+
+  addToFavorites(venueId) {
+    console.log("this is searchResults", this.state.searchResults);
+    // for(let i=0; i<this.state.searchResults.length; i++){
+    //   console.log('this is id', this.state.searchResults[i].id)
+    // }
+    //-----
+    let tempFav = this.state.favorites;
+    // console.log('this is indexof', tempFav.indexOf(venueId))
+    if (tempFav.indexOf(venueId) === -1) {
+      tempFav.push(venueId);
+      // console.log("Item's been favorited!");
+      // console.log(venueId);
+      axios
+        .post("/addfavorite", {
+          restaurant_id: venueId
+        })
+        // .then(res => console.log("Item's been favorited!"))
+        .then(this.setState({ favorites: tempFav }));
+      //  "background-blue"
+
+      // console.log("venueId ---->", venueId[0]);
+      console.log("this.state.favorites ---->", this.state.favorites);
+    } else {
+      let index = tempFav.indexOf(venueId);
+      tempFav.splice(index, 1);
+      axios
+        .delete("/removefavorite", {
+          restaurant_id: venueId
+        })
+        .then(this.setState({ favorites: tempFav }));
+    }
+    // console.log('this is tempFave length', tempFav.length)
   }
 
   // functions used for to select a specific venue on the category page to display on the venue page
   selectVenue(id, name, url, image, location, phone, latitude, longitude) {
+    // console.log(id);
     const venueId = id;
     const venueName = name;
     const venueUrl = url;
@@ -152,7 +196,7 @@ class MainContainer extends Component {
     const venueLatitude = latitude;
     const venueLongitude = longitude;
 
-    this.setState({ 
+    this.setState({
       loginPage: false,
       signupPage: false,
       homePage: false,
@@ -166,127 +210,136 @@ class MainContainer extends Component {
       venuePhone,
       venueLatitude,
       venueLongitude
-    })
+    });
   }
   setWaitTime(event) {
-    this.setState({ waitTime: event.target.value })
+    this.setState({ waitTime: event.target.value });
   }
   addWaitTime() {
     // create body from the things we've saved in state through the setwaittime and selectvenue func
     const body = {
       waitTime: this.state.waitTime,
       venueId: this.state.venueId,
-      venueName: this.state.venueName,
-    }
+      venueName: this.state.venueName
+    };
     // console.log(body);
-    fetch('/dbRouter/addWaitTime', {
-      method: 'POST',
+    fetch("/dbRouter/addWaitTime", {
+      method: "POST",
       body: JSON.stringify(body),
-      headers: {'Content-Type': 'application/json'}
+      headers: { "Content-Type": "application/json" }
     })
-    .then(() => console.log('addwaittime fetch request successful'))
-    .catch((err) => {
-      console.log(`${err}: addWaitTime function error when adding wait time`)
-    })
+      .then(() => console.log("addwaittime fetch request successful"))
+      .catch(err => {
+        console.log(`${err}: addWaitTime function error when adding wait time`);
+      });
   }
 
   render() {
     // conditional rendering for the login page
     let login = null;
     if (this.state.loginPage) {
-      login = 
-        <LoginPage 
-          signupButton = {this.signupButton}
-        />
+      login = <LoginPage signupButton={this.signupButton} />;
     }
-    
+
     // conditional rendering for the signup page
     let signup = null;
     if (this.state.signupPage) {
-      signup = 
-        <SignUpPage 
-          loginButton={this.loginButton}
-        />
+      signup = <SignUpPage loginButton={this.loginButton} />;
     }
 
     // conditional rendering for the homepage; default true (shows first)
     let home = null;
     if (this.state.homePage) {
-      document.body.style.background = "url('https://images.pexels.com/photos/1604200/pexels-photo-1604200.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
-      home = 
-      <div id="home-content">
-        {/* // uncomment to work on login and signup functionalities
+      document.body.style.background =
+        "url('https://images.pexels.com/photos/1604200/pexels-photo-1604200.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')";
+      home = (
+        <div id="home-content">
+          {/* // uncomment to work on login and signup functionalities
         <button onClick={this.loginButton}>Login</button> */}
-        <div id="logo">
-          <img id="logo-pic" src="https://image.flaticon.com/icons/png/512/876/876569.png"/>
-          <h1>Queue</h1>
+          <div id="logo">
+            <img
+              id="logo-pic"
+              src="https://image.flaticon.com/icons/png/512/876/876569.png"
+            />
+            <h1>Queue</h1>
+          </div>
+          <section id="home-page-search-bar">
+            <input
+              type="input"
+              id="searchInput"
+              placeholder="Business or Category"
+              onChange={this.setSearchInput}
+            />
+            <input
+              type="input"
+              id="location"
+              placeholder="Location"
+              onChange={this.setLocation}
+            />
+            <input type="button" id="searchButton" onClick={this.search} />
+          </section>
         </div>
-        <section id="home-page-search-bar">
-          <input type="input" id="searchInput" placeholder="Business or Category" onChange={this.setSearchInput}/>
-          <input type="input" id="location" placeholder="Location" onChange={this.setLocation}/>
-          <input type="button" id="searchButton" onClick={this.search}/>
-        </section>
-      </div>
-
+      );
     }
 
     // conditional rendering for the category page
     let category = null;
     if (this.state.categoryPage) {
       document.body.style.background = "url('')";
-      category = 
-      <CategoryContainer 
-        // props for search bar
-        setSearchInput = {this.setSearchInput}
-        setLocation = {this.setLocation}
-        search = {this.search}
-
-        searchInput={this.state.searchInput}
-        location={this.state.location}
-        searchResults={this.state.searchResults}
-
-        selectVenue={this.selectVenue}
-        waitTimes={this.state.waitTimes}
-
-        latitude={this.state.latitude}
-        longitude={this.state.longitude}
-
-        homePage={this.state.homePage}
-        categoryPage={this.state.categoryPage}
-        venuePage={this.state.venuePage}
-        current={this.state.current}
-      />
+      category = (
+        <CategoryContainer
+          // props for search bar
+          setSearchInput={this.setSearchInput}
+          setLocation={this.setLocation}
+          search={this.search}
+          searchInput={this.state.searchInput}
+          location={this.state.location}
+          searchResults={this.state.searchResults}
+          //-------
+          favorites={this.state.favorites}
+          addToFavorites={this.addToFavorites}
+          // favoriteColoring={this.state.favoriteColoring}
+          //--------
+          selectVenue={this.selectVenue}
+          waitTimes={this.state.waitTimes}
+          latitude={this.state.latitude}
+          longitude={this.state.longitude}
+          homePage={this.state.homePage}
+          categoryPage={this.state.categoryPage}
+          venuePage={this.state.venuePage}
+          current={this.state.current}
+        />
+      );
     }
 
     // conditional rendering for the venue page
-  let venue = null;
-  if (this.state.venuePage) {
-    venue = 
-    <VenueContainer
-      // props for search bar
-      setSearchInput = {this.setSearchInput}
-      setLocation = {this.setLocation}
-      search = {this.search}
+    let venue = null;
+    if (this.state.venuePage) {
+      venue = (
+        <VenueContainer
+          // props for search bar
+          setSearchInput={this.setSearchInput}
+          setLocation={this.setLocation}
+          search={this.search}
+          searchInput={this.state.searchInput}
+          location={this.state.location}
+          searchResults={this.state.searchResults}
+          // props for venue selection
+          venueId={this.state.venueId}
+          venueName={this.state.venueName}
+          venueUrl={this.state.venueUrl}
+          venueImage={this.state.venueImage}
+          venueLocation={this.state.venueLocation}
+          venuePhone={this.state.venuePhone}
+          venueWaitTimeList={this.state.venueWaitTimeList}
+          venueLatitude={this.state.venueLatitude}
+          venueLongitude={this.state.venueLongitude}
+          setWaitTime={this.setWaitTime}
+          addWaitTime={this.addWaitTime}
+        />
+      );
+    }
 
-      searchInput={this.state.searchInput}
-      location={this.state.location}
-      searchResults={this.state.searchResults}
-
-      // props for venue selection
-      venueId={this.state.venueId}
-      venueName={this.state.venueName}
-      venueUrl={this.state.venueUrl}
-      venueImage={this.state.venueImage}
-      venueLocation={this.state.venueLocation}
-      venuePhone={this.state.venuePhone}
-      venueWaitTimeList={this.state.venueWaitTimeList}
-      venueLatitude={this.state.venueLatitude}
-      venueLongitude={this.state.venueLongitude}
-      setWaitTime={this.setWaitTime}
-      addWaitTime={this.addWaitTime}
-    />
-  }
-    
     return (
       <div>
         {login}
@@ -295,7 +348,7 @@ class MainContainer extends Component {
         {category}
         {venue}
       </div>
-    )
+    );
   }
 }
 
