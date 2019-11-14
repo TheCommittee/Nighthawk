38e70420568b74278457a6b309646ba73f95d6d0
Talya Sasek <talyaercag@gmail.com>
2019-11-14 12:19:05 -0800

    CSS

--- src/containers/FavoritePageContainer.jsx
+++ src/containers/FavoritePageContainer.jsx
@@ -6,7 +6,14 @@ const FavoritePageContainer = props => {
   return (
     <div>
       <h2>My favorites</h2>
-      <div i='back-btn' onClick={()=>{props.backButton()}}>back</div>
+      <div
+        id="back-btn"
+        onClick={() => {
+          props.backButton();
+        }}
+      >
+        back
+      </div>
       <div className="favs-column">
         {props.favorites.map((el, i) => (
           <FavoriteElement
